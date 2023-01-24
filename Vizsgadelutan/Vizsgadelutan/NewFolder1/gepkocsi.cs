ef0d97f6c02f754b5523ba377b05512e74e1237f
Oszkár Nagy <oszkar.nagy@stud.uni-corvinus.hu>
2023-01-24 16:19:19 +0100

    első kész

---
+++ Vizsgadelutan/Vizsgadelutan/NewFolder1/gepkocsi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vizsgadelutan.NewFolder1
+{
+    class gepkocsi
+    {
+        public string Rendszam { get; set; }
+        public string Tipusnev { get; set; }
+        public int Evjarat { get; set; }
+        public bool Reszletre_is { get; set; }
+    }
+}
