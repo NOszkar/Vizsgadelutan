2c77a9cba00f45cb5ccf45314606f9495d6bd363
Oszkár Nagy <oszkar.nagy@stud.uni-corvinus.hu>
2023-01-24 16:51:23 +0100

    első.javitva

--- Vizsgadelutan/Vizsgadelutan/Form1.cs
+++ Vizsgadelutan/Vizsgadelutan/Form1.cs
@@ -3,19 +3,41 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Vizsgadelutan.NewFolder1;
 
 namespace Vizsgadelutan
 {
     public partial class Form1 : Form
     {
-        List<Gepkocsi.Gepkocsi> autok = new List<Gepkocsi.Gepkocsi>();
+        List<gepkocsi> autok = new List<gepkocsi>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void loadData()
+        {
+            using (StreamReader sr = new StreamReader("autok.csv", Encoding.Default))
+            {
+                sr.ReadLine(); // Header
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    var auto = new Gepkocsi.Gepkocsi();
+                    auto.Tipusnev = line[0];
+                    auto.Rendszam = line[1];
+                    auto.Evjarat = Int32.Parse(line[2]);
+                    auto.Reszletre_is = line[3] == "igen" ? true : false;
+
+                    autok.Add(auto);
+
+                }
+            }
         }
     }
 }
